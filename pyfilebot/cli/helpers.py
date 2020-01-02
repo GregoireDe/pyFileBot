fa6da9acf6ff9198c244078efef0aa561699b24f
GD <pea@invo.fr>
2020-01-02 01:34:11 +0100

    correct

--- pyfilebot/cli/helpers.py
+++ pyfilebot/cli/helpers.py
@@ -7,15 +7,15 @@ from pyfilebot.utils import Files
 CONTEXT_SETTINGS = {'context_settings': dict(help_option_names=['-h', '--help'])}
 
 DEFAULT_RULES = {
-    "movies": "{n} ({y})/{n} ({y}).{x}",
-    "shows": "{n}/Season {s}/{n} - S{s00}E{e00} - {t}.{x}"
+    "movies": "{t} ({y})/{t} ({y}).{x}",
+    "shows": "{t}/Season {s}/{t} - S{s00}E{e00} - {n}.{x}"
 }
 
 
 def do_rename(filepath, filename, **args):
     try:
         file = args['cls'](filename, args['ignore'], args['language'], args['cache'])
-        new_name = Files.process_rules(args['rules'], "ShowEpisode", file.__dict__)
+        new_name = Files.process_rules(args['rules'], args['cls'].__name__, file.__dict__)
         Files.rename(filepath, args['output'], new_name, args['force'], args['action'], args['dry_run'])
     except Exception as e:
         print(e)
